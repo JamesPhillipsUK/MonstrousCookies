bf3fcc272385a0f0d546b18fab00121499fa2cb1
James Phillips <JamesPhillipsUK@users.noreply.github.com>
2017-02-08 22:22:43 +0000

    Added JavaScriptNotice.php
    JavaScriptNotice.php is a nifty little optional extra that allows the site owner to inject a line into the HTML of their site on load that notifies users accessing the site without JavaScript enabled that it requires JavaScript...  Because all modern sites do.

---
+++ JavaScriptNotice.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * MonstrousCookies, created by James Phillips <james@jamesphillipsuk.com> September 2016.  
+ * MonstrousCookies is a PHP plugin to allow sites to comply with the EU Cookie Law by informing users of a site's cookie policy.
+ * JavaScriptNotice.php is a part of MonstrousCookies.
+ * 
+ * Copyright (C) 2016 James Phillips, Released under the GNU GPL v3.0 or later.
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms of the 
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the License, 
+ * or (at your option) any later version.  This program is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or 
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.   
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * 
+**/
+	echo '<noscript>This Website uses JavaScript to improve your user experience.  Please turn JavaScript on before continuing.</noscript>';
+?>
